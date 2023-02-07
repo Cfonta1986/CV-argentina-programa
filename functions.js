003742f7f1baa0a50157dc2a6b8389a3e8ce0812
Cristian Fontanini <cristianfonta16@gmail.com>
2023-02-07 06:30:58 -0300

    agregar jquery

--- functions.js
+++ functions.js
@@ -1,5 +1,7 @@
-function cambia(valor,ancho,alto)
-{
-document.all(valor).width = ancho;
-document.all(valor).height = alto;
-}
+$(document).ready(function(){
+    $('.foto-cv').hover(function() {
+        $(this).addClass('transition');
+    }, function() {
+        $(this).removeClass('transition');
+    });
+});
